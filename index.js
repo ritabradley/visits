e4a39ec133e3c825163e4a4dc671d7ca112e5547
Rita Bradley <rita@ritabradley.dev>
2023-04-19 19:17:03 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cli = require('nodemon/lib/cli');
+const redis = require('redis');
+
+const app = express();
+
+const PORT = 5000;
+
+const client = redis.createClient({});
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send(`Number of visits: ${visits}`);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Listening on port 5000');
+});
